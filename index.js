4ce04efa13739595bb32f8c849f787a51fb97d9c
Jeffrey Davis <j.davis081248@gmail.com>
2023-10-26 18:28:40 -0500

    Arrays Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name)
+{
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat()
+{
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat()
+{
+    cats.shift();
+}
+
+function appendCat(name)
+{
+    const catsNew = [...cats, name];
+    return catsNew;
+}
+
+function prependCat(name)
+{
+    const catsNew = [name, ...cats];
+    return catsNew;
+}
+
+function removeLastCat()
+{
+    const catsNew = [...cats];
+    catsNew.pop();
+    return catsNew;
+}
+
+function removeFirstCat()
+{
+    const catsNew = [...cats];
+    catsNew.shift();
+    return catsNew;
+}
